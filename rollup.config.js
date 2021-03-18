6a4321c774a8509d131969c98604b00e6a4206b6
riccardoaranha <riccardoaranha@gmail.com>
2021-03-18 04:25:06 +0100

    Improvements on npm build, converting to js, instead of typescript. On branch main Your branch is up to date with 'origin/main'. Changes to be committed: 	modified:   .gitignore 	modified:   package-lock.json 	modified:   package.json 	new file:   rollup.config.js 	modified:   tsconfig.json

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import dts from 'rollup-plugin-dts';
+
+export default [
+    {
+        input: 'dist/index.js',
+        output: {
+            file: 'index.js'
+        }
+    },
+    {
+        input: 'dist/index.d.ts',
+        output: {
+            file: 'index.d.ts'
+        },
+        plugins: [dts()],
+        external: ['util']
+    }
+]
